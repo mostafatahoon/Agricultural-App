c353de2d9654fff52b7bf4c93b9e76bcc9ab3ff8
Admin <eslamatya843@gmail.com>
2026-04-06 17:35:19 +0200

    presentation ui setting theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,10 @@ android {
     buildFeatures {
         compose = true
     }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+        unitTests.all { it.enabled = false }
+    }
 }
 
 dependencies {
